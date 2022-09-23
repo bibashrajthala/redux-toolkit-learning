9e01f1ae5be82895d958c945152d201f2f3f92b1
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-09-23 11:32:34 +0545

    make a counter app using redux-toolkit

---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  const [amountToAdd, setAmountToAdd] = useState(0);
+  const addValue = Number(amountToAdd);
+
+  const resetAll = () => {
+    setAmountToAdd(0);
+    dispatch(reset());
+  };
+  return (
+    <section>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+
+      <input
+        type="number"
+        onChange={(e) => setAmountToAdd(e.target.value)}
+        value={amountToAdd}
+      />
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>
+          Add Amount
+        </button>
+        <button onClick={resetAll}>Reset</button>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
